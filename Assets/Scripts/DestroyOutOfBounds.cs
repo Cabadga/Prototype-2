54abebefbd15f446f2d4000ea5faef1ed203f278
Cabadga <121972248+Cabadga@users.noreply.github.com>
2023-02-01 11:03:58 -0800

    2.4 Completed

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -16,6 +16,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("You Suck!");
             Destroy(gameObject);
         }
     }
